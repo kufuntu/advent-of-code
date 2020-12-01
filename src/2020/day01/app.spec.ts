6ca9a077143fd2bace4184db8f3cb7c647c81bde
Andre Steudel <andre.steudel@yellowmap.de>
2020-12-01 11:32:21 +0100

    day01 solution

--- src/2020/day01/app.spec.ts
+++ src/2020/day01/app.spec.ts
@@ -1,5 +1,29 @@
+import { calculateExpenseReport1, calculateExpenseReport2, splitInput } from './app';
+
+const testData = `1721
+979
+366
+299
+675
+1456`;
+
+const splitTestData = [
+  1721,
+  979,
+  366,
+  299,
+  675,
+  1456
+];
+
 describe('Main entry', () => {
-  it('contains a create function', () => {
-    expect(true).toBeDefined();
+  it('returns the split input for the testData', () => {
+    expect(splitInput(testData)).toEqual(splitTestData);
+  });
+  it('returns for two interations 514579', () => {
+    expect(calculateExpenseReport1(splitTestData)).toEqual(514579);
+  });
+  it('returns for three interations 241861950', () => {
+    expect(calculateExpenseReport2(splitTestData)).toEqual(241861950);
   });
 });
--- src/2020/day01/app.ts
+++ src/2020/day01/app.ts
@@ -1,3 +1,48 @@
-import { readInput } from "../../util";
+import { readFileSync } from 'fs';
 
-console.log(readInput("input.txt"));
+const file = readFileSync('./src/2020/day01/input.txt').toString();
+
+export const splitInput = (input: string) => input.split("\n").map(number => parseInt(number.trim(), 10));
+
+export const calculateExpenseReport1 = (input: number[], year = 2020) => {
+  const foundNumbers: number[] = [];
+  outer_loop:
+  for (var i = 0; i < input.length; i++) {
+    for (var j = 0; j < input.length; j++) {
+        const first = input[i];
+        const second = input[j];
+        if (first + second === year) {
+          foundNumbers.push(first);
+          foundNumbers.push(second);
+          break outer_loop;
+        }
+    }
+  }
+  return foundNumbers.reduce((acc, number) => acc * number, 1);
+};
+
+export const calculateExpenseReport2 = (input: number[], year = 2020) => {
+  const foundNumbers: number[] = [];
+  outer_loop:
+  for (var i = 0; i < input.length; i++) {
+    for (var j = 0; j < input.length; j++) {
+      for (var k = 0; k < input.length; k++) {
+        const first = input[i];
+        const second = input[j];
+        const third = input[k];
+        if (first + second + third === year) {
+          foundNumbers.push(first);
+          foundNumbers.push(second);
+          foundNumbers.push(third);
+          break outer_loop;
+        }
+      }
+    }
+  }
+  return foundNumbers.reduce((acc, number) => acc * number, 1);
+};
+
+const split = splitInput(file);
+const resultPart1 = calculateExpenseReport1(split);
+const resultPart2 = calculateExpenseReport2(split);
+console.log(resultPart1, resultPart2);
