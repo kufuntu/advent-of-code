f6aafc1308cac669a01f04e12cb4342cf2b5eed2
Andre Steudel <andre.steudel@yellowmap.de>
2020-12-01 11:34:20 +0100

    prepare day02

---
+++ src/2020/day02/app.spec.ts
@@ -0,0 +1,7 @@
+const testData = ``;
+
+describe('Main entry', () => {
+  /*it('returns the split input for the testData', () => {
+    expect(splitInput(testData)).toEqual(splitTestData);
+  });*/
+});
