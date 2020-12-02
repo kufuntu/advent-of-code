7fd4a7af43d587b42c11fe3511b915cad246e006
Andre Steudel <kontakt@andre-steudel.de>
2020-12-02 11:10:41 +0100

    day02 solution

--- src/2020/day02/app.spec.ts
+++ src/2020/day02/app.spec.ts
@@ -1,7 +1,23 @@
-const testData = ``;
+import { checkPasswords, checkPasswords2, parsePasswordWithRules, PasswordWithRule } from "./app";
+
+const testData = `1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc`;
+
+const parsedData: PasswordWithRule[] = [
+	{ min: 1, max: 3, char: 'a', password: 'abcde'},
+	{ min: 1, max: 3, char: 'b', password: 'cdefg'},
+	{ min: 2, max: 9, char: 'c', password: 'ccccccccc'},
+];
 
 describe('Main entry', () => {
-  /*it('returns the split input for the testData', () => {
-    expect(splitInput(testData)).toEqual(splitTestData);
-  });*/
+  it('returns the parsed data for the testData', () => {
+    expect(parsePasswordWithRules(testData)).toEqual(parsedData);
+	});
+	it('returns number of valid passwords', () => {
+		expect(checkPasswords(parsedData)).toEqual(2);
+	})
+	it('returns number of valid passwords for second algo', () => {
+		expect(checkPasswords2(parsedData)).toEqual(1);
+	})
 });
--- src/2020/day02/app.ts
+++ src/2020/day02/app.ts
@@ -2,9 +2,49 @@ import { readFileSync } from 'fs';
 
 const file = readFileSync('./src/2020/day02/input.txt').toString();
 
-export const splitInput = (input: string) => input.split("\n").map(number => parseInt(number.trim(), 10));
+export interface PasswordWithRule {
+	min: number;
+	max: number;
+	char: string;
+	password: string;
+}
 
-const split = splitInput(file);
-//const resultPart1 = calculateExpenseReport1(split);
-//const resultPart2 = calculateExpenseReport2(split);
-//console.log(resultPart1, resultPart2);
+export const parsePasswordWithRules = (input: string): PasswordWithRule[] => {
+	const lines = input.split("\n").map(line => line.trim());
+	const regex = /([0-9]*)-([0-9]*) ([a-zA-Z])*: ([a-zA-Z]*)/;
+	return lines.map(line => {
+		// 1-3 b: cdefg
+		const parts = regex.exec(line);
+		return {
+			min: parseInt(parts[1]),
+			max: parseInt(parts[2]),
+			char: parts[3],
+			password: parts[4]
+		} as PasswordWithRule;
+	})
+};
+
+export const checkPasswords = (passwords: PasswordWithRule[]): number => {
+	const valid = passwords.filter(rule => {
+		const regex = new RegExp(rule.char, 'g');
+		const matches = rule.password.match(regex);
+		const num = matches ? matches.length : 0;
+		return num >= rule.min && num <= rule.max;
+	});
+	return valid.length;
+}
+
+export const checkPasswords2 = (passwords: PasswordWithRule[]): number => {
+	const valid = passwords.filter(rule => {
+		const firstChar = rule.password.charAt(rule.min - 1);
+		const secondChar = rule.password.charAt(rule.max - 1);
+		return (firstChar === rule.char && secondChar != rule.char)
+			|| (firstChar != rule.char && secondChar === rule.char);
+	});
+	return valid.length;
+}
+
+const rules = parsePasswordWithRules(file);
+const resultPart1 = checkPasswords(rules);
+const resultPart2 = checkPasswords2(rules);
+console.log(resultPart1, resultPart2);
