1864cbe7b8f06325f2ca53b0780d327b045b0d8a
Andre Steudel <andre.steudel@yellowmap.de>
2020-12-04 11:26:43 +0100

    prepare day04

---
+++ src/2020/day04/app.spec.ts
@@ -0,0 +1,12 @@
+import { splitInput } from './app';
+
+const testData = ``;
+
+const splitTestData = [
+];
+
+describe('Main entry', () => {
+  it('returns the split input for the testData', () => {
+    expect(splitInput(testData)).toEqual(splitTestData);
+  });
+});
---
+++ src/2020/day04/app.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from 'fs';
+
+const file = readFileSync('./src/2020/day04/input.txt').toString();
+
+export const splitInput = (input: string) => input.split("\n").map(number => parseInt(number.trim(), 10));
+
+/*const split = splitInput(file);
+const resultPart1 = calculateExpenseReport1(split);
+const resultPart2 = calculateExpenseReport2(split);
+console.log(resultPart1, resultPart2);*/
