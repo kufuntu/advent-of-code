c973894776e4254e817647906a2bdb3c1a852abd
Andre Steudel <andre.steudel@yellowmap.de>
2020-12-04 12:57:44 +0100

    prepare other days

---
+++ src/2020/day14/app.ts
@@ -0,0 +1,11 @@
+import { readFileSync } from 'fs';
+
+const file = readFileSync('./src/2020/day14/input.txt').toString();
+
+export const splitInput = (input: string) => input.split("\n").map(number => parseInt(number.trim(), 10));
+
+/*
+const split = splitInput(file);
+const resultPart1 = calculateExpenseReport1(split);
+const resultPart2 = calculateExpenseReport2(split);
+console.log(resultPart1, resultPart2);*/
