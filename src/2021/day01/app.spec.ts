a297a23083abf65d30957da2b00ba8bcb09793fc
Andre Steudel <andre.steudel@yellowmap.de>
2021-12-13 14:01:14 +0100

    solution for day1

--- src/2021/day01/app.spec.ts
+++ src/2021/day01/app.spec.ts
@@ -1,11 +1,51 @@
-import { splitInput } from './app';
+import { measureDepth, measureDepth2, splitInput } from './app';
 
-const testData = ``;
+const testData = `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`;
 
-const splitTestData = [];
+const splitTestData = [
+  199,
+  200,
+  208,
+  210,
+  200,
+  207,
+  240,
+  269,
+  260,
+  263,
+];
+
+const splitTestData2 = [
+  607,
+  618,
+  618,
+  617, 
+  647,
+  716,
+  769,
+  792,
+];
 
 describe('Main entry', () => {
   it('returns the split input for the testData', () => {
     expect(splitInput(testData)).toEqual(splitTestData);
+  });
+  it('returns the correct depth', () => {
+    expect(measureDepth(splitTestData)).toEqual(7);
+  });
+  it('returns the correct depth2', () => {
+    expect(measureDepth(splitTestData2)).toEqual(5);
+  });
+  it('returns the correct depth3', () => {
+    expect(measureDepth2(splitTestData)).toEqual(5);
   });
 });
--- src/2021/day01/app.ts
+++ src/2021/day01/app.ts
@@ -4,8 +4,27 @@ const file = readFileSync('./src/2021/day01/input.txt').toString();
 
 export const splitInput = (input: string) => input.split("\n").map(number => parseInt(number.trim(), 10));
 
-/*
+export const measureDepth = (arr: number[]) => {
+  let start = arr[0];
+
+  return arr.reduce((acc, cur) => {
+    if (cur > start) {
+      acc++;
+    }
+    start = cur;
+    return acc;
+  }, 0);
+};
+
+export const measureDepth2 = (arr: number[]) => {
+  const sum = [];
+  for (var i = 0; i <= arr.length - 3; i++) {
+    sum.push(arr[i] + arr[i+1] + arr[i+2]);
+  }
+  return measureDepth(sum);
+};
+
 const split = splitInput(file);
-const resultPart1 = calculateExpenseReport1(split);
-const resultPart2 = calculateExpenseReport2(split);
-console.log(resultPart1, resultPart2);*/
+const resultPart1 = measureDepth(split);
+const resultPart2 = measureDepth2(split);
+console.log(resultPart1, resultPart2);
