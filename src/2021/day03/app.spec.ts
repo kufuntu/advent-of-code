e3db149a21b6a26ff260c155f15dc0fc7009d312
Andre Steudel <andre.steudel@yellowmap.de>
2021-12-13 15:05:51 +0100

    solution for day3

--- src/2021/day03/app.spec.ts
+++ src/2021/day03/app.spec.ts
@@ -1,11 +1,58 @@
-import { splitInput } from './app';
+import { co2, epsilon, gamma, oxygen, pair, splitInput } from './app';
 
-const testData = ``;
+const testData = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`;
 
-const splitTestData = [];
+const splitTestData = [
+  "00100",
+  "11110",
+  "10110",
+  "10111",
+  "10101",
+  "01111",
+  "00111",
+  "11100",
+  "10000",
+  "11001",
+  "00010",
+  "01010"
+];
+
+const pairs = [
+  [0,1,1,1,1,0,0,1,1,1,0,0],
+  [0,1,0,0,0,1,0,1,0,1,0,1],
+  [1,1,1,1,1,1,1,1,0,0,0,0],
+  [0,1,1,1,0,1,1,0,0,0,1,1],
+  [0,0,0,1,1,1,1,0,0,1,0,0]
+];
 
 describe('Main entry', () => {
   it('returns the split input for the testData', () => {
     expect(splitInput(testData)).toEqual(splitTestData);
+  });
+  it('returns the split pairs for the testData', () => {
+    expect(pair(splitTestData)).toEqual(pairs);
+  });
+  it('returns the gamma', () => {
+    expect(gamma(pairs)).toEqual(22);
+  });
+  it('returns the epsilon', () => {
+    expect(epsilon(pairs)).toEqual(9);
+  });
+  it('returns the oxygen', () => {
+    expect(oxygen(splitTestData)).toEqual(23);
+  });
+  it('returns the co2', () => {
+    expect(co2(splitTestData)).toEqual(10);
   });
 });
--- src/2021/day03/app.ts
+++ src/2021/day03/app.ts
@@ -2,10 +2,69 @@ import { readFileSync } from 'fs';
 
 const file = readFileSync('./src/2021/day03/input.txt').toString();
 
-export const splitInput = (input: string) => input.split("\n").map(number => parseInt(number.trim(), 10));
+export const splitInput = (input: string) => input.split("\n").map(number => number.trim());
+export const pair = (lines: string[]) => {
+  const arr = [];
+  lines.forEach(line => {
+    const chars = line.split("");
+    for (var i = 0; i < chars.length; i++) {
+      if (!arr[i]) {
+        arr[i] = [];
+      }
+      arr[i].push(parseInt(chars[i], 10));
+    }
+  });
+  return arr;
+};
 
-/*
+export const gamma = (pair: number[][]) => {
+  const num = pair.reduce((acc, cur) => {
+    const result = cur.filter(num => num === 0).length > cur.length / 2 ? '0' : '1';
+    return acc + result;
+  }, '');
+  return parseInt(num, 2);
+};
+
+export const epsilon = (pair: number[][]) => {
+  const num = pair.reduce((acc, cur) => {
+    const result = cur.filter(num => num === 0).length > cur.length / 2 ? '1' : '0';
+    return acc + result;
+  }, '');
+  return parseInt(num, 2);
+};
+
+export const oxygen = (split: string[]) => {
+  let lines = split;
+
+  for (let i = 0; i < split[0].length; i++) {
+    const cur = pair(lines);
+    const result = cur[i].filter(num => num === 0).length > cur[i].length / 2 ? '0' : '1';
+    lines = lines.filter(line => {
+      return line.charAt(i) == result;
+    });
+    if (lines.length === 1) break;
+  }
+
+  return parseInt(lines[0], 2);
+};
+
+export const co2 = (split: string[]) => {
+  let lines = split;
+
+  for (let i = 0; i < split[0].length; i++) {
+    const cur = pair(lines);
+    const result = cur[i].filter(num => num === 0).length > cur[i].length / 2 ? '1' : '0';
+    lines = lines.filter(line => {
+      return line.charAt(i) == result;
+    });
+    if (lines.length === 1) break;
+  }
+
+  return parseInt(lines[0], 2);
+};
+
 const split = splitInput(file);
-const resultPart1 = calculateExpenseReport1(split);
-const resultPart2 = calculateExpenseReport2(split);
-console.log(resultPart1, resultPart2);*/
+const pairs = pair(split);
+const resultPart1 = epsilon(pairs) * gamma(pairs);
+const resultPart2 = oxygen(split) * co2(split);
+console.log(resultPart1, resultPart2);
