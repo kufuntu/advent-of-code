220c9d495b6d8f63283c4bfd12d7c2610b135ab0
Andre Steudel <andre.steudel@yellowmap.de>
2021-12-13 16:01:22 +0100

    solution for day4

--- src/2021/day04/app.spec.ts
+++ src/2021/day04/app.spec.ts
@@ -1,11 +1,61 @@
-import { splitInput } from './app';
+import { checkWinner, play, play2, splitGrids, splitInput } from './app';
 
-const testData = ``;
+const testData = `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
-const splitTestData = [];
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7`;
+
+const splitTestData = [
+  7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+];
+
+const grids = splitGrids(testData);
 
 describe('Main entry', () => {
   it('returns the split input for the testData', () => {
     expect(splitInput(testData)).toEqual(splitTestData);
+  });
+  it('returns if a grid has won horizontal', () => {
+    expect(checkWinner([
+      [
+        [ { checked: true, num: 1 }, { checked: true, num: 1 }, { checked: true, num: 1 }, { checked: true, num: 1 }, { checked: true, num: 1 } ],
+        [ { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 } ],
+        [ { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 } ],
+        [ { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 } ],
+        [ { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 } ],
+      ]
+    ])).toEqual(0);
+  });
+  it('returns if a grid has won vertical', () => {
+    expect(checkWinner([
+      [
+        [ { checked: true, num: 1 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 } ],
+        [ { checked: true, num: 1 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 } ],
+        [ { checked: true, num: 1 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 } ],
+        [ { checked: true, num: 1 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 } ],
+        [ { checked: true, num: 1 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 }, { checked: false, num: 0 } ],
+      ]
+    ])).toEqual(0);
+  });
+  it('returns the won game', () => {
+    expect(play(grids, splitTestData)).toEqual(4512);
+  });
+  it('returns the won game part2', () => {
+    expect(play2(grids, splitTestData)).toEqual(1924);
   });
 });
--- src/2021/day04/app.ts
+++ src/2021/day04/app.ts
@@ -2,10 +2,113 @@ import { readFileSync } from 'fs';
 
 const file = readFileSync('./src/2021/day04/input.txt').toString();
 
-export const splitInput = (input: string) => input.split("\n").map(number => parseInt(number.trim(), 10));
+export type Grid = {
+  checked: boolean;
+  num: number;
+}[][];
 
-/*
+export const splitInput = (input: string) => input.split("\n")[0].split(',').map(number => parseInt(number.trim(), 10));
+export const splitGrids = (input: string): Grid[] => {
+  const grids = input.split("\n\n");
+  grids.shift();
+  return grids.map(grid => grid.split("\n").map(line => line.trim().split(/\s{1,}/).map(num => ({
+    checked: false,
+    num: parseInt(num, 10)
+  }))));
+};
+
+export const setNumber = (grids: Grid[], value: number) => {
+  return grids.map(grid => grid.map(grid2 => grid2.map(val => {
+    if (val.num === value) {
+      val.checked = true;
+    }
+    return val;
+  })));
+};
+
+export const checkWinner = (grids: Grid[]) => {
+  return grids.filter((grid) => {
+    let found = false;
+    for (let i = 0; i < grid.length; i++) {
+      let horizontal = true;
+      let vertical = true;
+      for (let j = 0; j < grid.length; j++) {
+        if (!grid[i][j].checked) {
+          horizontal = false;
+        }
+        if (!grid[j][i].checked) {
+          vertical = false;
+        }
+      }
+      if (horizontal || vertical) {
+        found = true;
+        break;
+      }
+    }
+    return found;
+  });
+};
+
+export const print = (grid: Grid) => {
+  console.log(grid.map(grid2 => {
+    return grid2.reduce((acc, cur) => {
+      if (cur.checked) {
+        acc += ' ['+cur.num+']';
+      } else {
+        acc += ' ' + cur.num;
+      }
+      return acc;
+    }, "");
+  }));
+}
+
+export const play = (grids: Grid[], nums: number[]) => {
+  let check;
+  let currentNumber;
+  for(let i = 0; i < nums.length; i++) {
+    currentNumber = nums[i];
+    grids = setNumber(grids, currentNumber);
+    check = checkWinner(grids);
+    if (check != -1) {
+      break;
+    }
+  }
+  let grid = grids[check];
+  let unchecked = grid.reduce((acc, val) => {
+    return val.reduce((acc, num) => {
+      if (!num.checked) acc += num.num;
+      return acc;
+    }, acc)
+  }, 0);
+  return unchecked * currentNumber;
+};
+
+export const play2 = (grids: Grid[], nums: number[]) => {
+  let currentNumber;
+  let grid;
+  for(let i = 0; i < nums.length; i++) {
+    currentNumber = nums[i];
+    grids = setNumber(grids, currentNumber);
+    let found = checkWinner(grids);
+    if (found.length > 0) {
+      grid = found[0];
+      grids = grids.filter((value, index) => !found.find(f => f === value));
+      console.log(grids.length);
+    }
+    if (grids.length == 0) {
+      break;
+    }
+  }
+  let unchecked = grid.reduce((acc, val) => {
+    return val.reduce((acc, num) => {
+      if (!num.checked) acc += num.num;
+      return acc;
+    }, acc)
+  }, 0);
+  return unchecked * currentNumber;
+};
+
 const split = splitInput(file);
-const resultPart1 = calculateExpenseReport1(split);
-const resultPart2 = calculateExpenseReport2(split);
-console.log(resultPart1, resultPart2);*/
+const grids = splitGrids(file);
+const resultPart2 = play2(grids, split);
+console.log(resultPart2);
