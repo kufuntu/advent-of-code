d611656bcd7c9779470053db8bcb210599a23d5a
Andre Steudel <andre.steudel@yellowmap.de>
2021-12-14 15:29:33 +0100

    solution for day5

--- src/2021/day05/app.spec.ts
+++ src/2021/day05/app.spec.ts
@@ -1,11 +1,40 @@
-import { splitInput } from './app';
+import { fillVents, findLargest, splitInput } from './app';
 
-const testData = ``;
+const testData = `0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2`;
 
-const splitTestData = [];
+const splitTestData = [
+  { x1: 0, y1: 9, x2: 5, y2: 9 },
+  { x1: 8, y1: 0, x2: 0, y2: 8 },
+  { x1: 9, y1: 4, x2: 3, y2: 4 },
+  { x1: 2, y1: 2, x2: 2, y2: 1 },
+  { x1: 7, y1: 0, x2: 7, y2: 4 },
+  { x1: 6, y1: 4, x2: 2, y2: 0 },
+  { x1: 0, y1: 9, x2: 2, y2: 9 },
+  { x1: 3, y1: 4, x2: 1, y2: 4 },
+  { x1: 0, y1: 0, x2: 8, y2: 8 },
+  { x1: 5, y1: 5, x2: 8, y2: 2 },
+];
 
 describe('Main entry', () => {
   it('returns the split input for the testData', () => {
     expect(splitInput(testData)).toEqual(splitTestData);
+  });
+  it('returns vents', () => {
+    expect(fillVents(splitTestData)[0]).toEqual([undefined, undefined, undefined, undefined, undefined, undefined, undefined, 1]);
+  });
+  it('returns result', () => {
+    expect(findLargest(fillVents(splitTestData))).toEqual(5);
+  });
+  it('returns vents2', () => {
+    expect(fillVents(splitTestData, true)[0]).toEqual([1, undefined, 1, undefined, undefined, undefined, undefined, 1, 1]);
   });
 });
--- src/2021/day05/app.ts
+++ src/2021/day05/app.ts
@@ -1,11 +1,103 @@
 import { readFileSync } from 'fs';
 
 const file = readFileSync('./src/2021/day05/input.txt').toString();
+const regex = /(\d*),(\d*) -> (\d*),(\d*)/;
 
-export const splitInput = (input: string) => input.split("\n").map(number => parseInt(number.trim(), 10));
+export const splitInput = (input: string) => input.split("\n").map(line => {
+  const result = regex.exec(line);
+  return {
+    x1: parseInt(result[1], 10),
+    y1: parseInt(result[2], 10),
+    x2: parseInt(result[3], 10),
+    y2: parseInt(result[4], 10),
+  } as Vent;
+});
 
-/*
+export interface Vent {
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
+}
+
+export const print = (numbers: number[][]) => {
+  console.log(numbers.reduce((acc, line) => {
+    for (let i = 0; i < line.length; i++) {
+      if (!line[i]) acc += '.'
+      else acc += line[i];
+    }
+    return acc + "\n";
+  }, ""));
+};
+
+export const fillVents = (vents: Vent[], diagonal: boolean = false): number[][] => {
+  const arr: number[][] = [];
+  vents.forEach(vent => {
+    if (vent.x1 === vent.x2) {
+      const start = vent.y1 > vent.y2 ? vent.y2 : vent.y1;
+      const end = vent.y1 > vent.y2 ? vent.y1 : vent.y2;
+      for (let i = start; i <= end; i++) {
+        if (!arr[i]) {
+          arr[i] = [];
+        }
+        if (!arr[i][vent.x1]) {
+          arr[i][vent.x1] = 1;
+        } else {
+          arr[i][vent.x1]++;
+        }
+      }
+    } else if (vent.y1 === vent.y2) {
+      const start = vent.x1 > vent.x2 ? vent.x2 : vent.x1;
+      const end = vent.x1 > vent.x2 ? vent.x1 : vent.x2;
+      for (let i = start; i <= end; i++) {
+        if (!arr[vent.y1]) {
+          arr[vent.y1] = [];
+        }
+        if (!arr[vent.y1][i]) {
+          arr[vent.y1][i] = 1;
+        } else {
+          arr[vent.y1][i]++;
+        }
+      }
+    } else if (diagonal) {
+      for (let i = 0; i <= Math.abs(vent.y1 - vent.y2); i++) {
+        let x = vent.x1, y = vent.y1;
+        if (vent.x1 > vent.x2) {
+          x -= i;
+        } else {
+          x += i;
+        }
+        if (vent.y1 > vent.y2) {
+          y -= i;
+        } else {
+          y += i;
+        }
+        if (!arr[y]) {
+          arr[y] = [];
+        }
+        if (!arr[y][x]) {
+          arr[y][x] = 1;
+        } else {
+          arr[y][x]++;
+        }
+      }
+    }
+  });
+  return arr;
+};
+
+export const findLargest = (arr: number[][]) => {
+  return arr.reduce((acc, cur) => {
+    cur.map(num => {
+      if (num >= 2) {
+        acc++;
+      }
+    });
+    return acc;
+  }, 0);
+};
+
 const split = splitInput(file);
-const resultPart1 = calculateExpenseReport1(split);
-const resultPart2 = calculateExpenseReport2(split);
-console.log(resultPart1, resultPart2);*/
+const resultPart1 = findLargest(fillVents(split));
+const resultPart2 = findLargest(fillVents(split, true));
+console.log(resultPart1, resultPart2);
